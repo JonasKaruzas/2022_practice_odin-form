b47a4c2eb0bac8b2729a86510795f7bac2eeb0cd
JonasKaruzas <jonas.karuzas@gmail.com>
2022-09-26 21:33:25 +0300

    style: :lipstick: media quaries

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-// Time to script!
+const createAccountForm = document.getElementById("createAccountForm");
+const passwordError = document.getElementById("password-error");
+const passwordEl = document.querySelector("#createAccountForm #password");
+const passwordContEl = document.querySelector(
+  "#createAccountForm #passwordConfirm"
+);
+
+function displayError() {
+  passwordError.style.display = "block";
+  passwordEl.classList.add("error");
+  passwordContEl.classList.add("error");
+}
+
+function removeError() {
+  passwordError.style.display = "none";
+  passwordEl.classList.remove("error");
+  passwordContEl.classList.remove("error");
+}
+
+function passwordCheck(e) {
+  e.preventDefault();
+
+  const mainPassword = createAccountForm.elements.password.value;
+  const confirmPassword = createAccountForm.elements.passwordConfirm.value;
+
+  if (mainPassword !== confirmPassword) {
+    displayError();
+  } else {
+    removeError();
+  }
+}
+
+createAccountForm.addEventListener("submit", passwordCheck);
